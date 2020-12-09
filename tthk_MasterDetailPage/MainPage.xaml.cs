6e43cfef02979b1c466f93f6cab90c6cbd8dde5c
Opilane TTHK <opilane@tthk.ee>
2020-12-09 11:05:26 +0200

    Add project files.

---
+++ tthk_MasterDetailPage/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace tthk_MasterDetailPage
+{
+    public partial class MainPage : MasterDetailPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            profileImage.Source = ImageSource.FromFile("");
+            aboutList.ItemsSource = GetMenuList();
+
+        }
+    }
+}
---
+++ tthk_MasterDetailPage/View/Sasuke.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace tthk_MasterDetailPage.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Sasuke : ContentPage
+    {
+        public Sasuke()
+        {
+            InitializeComponent();
+        }
+    }
+}
